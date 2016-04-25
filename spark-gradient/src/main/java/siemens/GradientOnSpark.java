f695540942920eea65f5d0b409afb1397bc2681c
svetasvesh <tsvetujochka@yandex.ru>
2016-04-25 09:44:05 +0300

    Черновая попытка сделать вывод в файл. Пока что файл создается в директории, из который был произведен запуск программы. Надо - чтобы в директории с программой.

--- spark-gradient/src/main/java/siemens/GradientOnSpark.java
+++ spark-gradient/src/main/java/siemens/GradientOnSpark.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.*;
 import java.util.Arrays;
 import java.util.List;
+import java.io.File;
+import java.io.PrintWriter;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -13,31 +15,35 @@ import org.apache.spark.api.java.function.VoidFunction;
 
 public class GradientOnSpark{
 
+	//initial conditions
+	final static double a = -10;
+	final static double b = 10;
+	final static double c = -10; 
+	final static double d = 10;
+	final static double lambda = 0.01;
+	final static int grid = 10;
+
+	//кол-во элементов по каждой стороне матрицы
+	static int xcount = (int) ((b-a)/lambda);
+	static int ycount = (int) ((d-c)/lambda);
+
+	//кол-во элементов по каждой стороне блока
+	static int xgrid = (int) xcount/grid;
+	static int ygrid = (int) ycount/grid;
+
+	static double minvalue;
+	static double imin, jmin;
+
 	public static void main(String[] args) {
 		//Parametres:
 		//f(x,y) = (x+2)*(x+2) + y*y
 		//x: [-10; 10]
 		//y[-10; 10]
 		//lambda = 0.01;
-
-		//initial conditions
-		final double a = -10;
-		final double b = 10;
-		final double c = -10; 
-		final double d = 10;
-		final double lambda = 0.01;
-		final int grid = 10;
-		double minvalue;
-		double imin, jmin;
-
-		//кол-во элементов по каждой стороне матрицы
-		int xcount = (int) ((b-a)/lambda);
-		int ycount = (int) ((d-c)/lambda);
 		
+		System.out.println("Create matrix " + xcount + "*" + ycount + " elements and " + grid + "*" + grid + " blocks" );
 
-		//кол-во элементов по каждой стороне блока
-		int xgrid = (int) xcount/grid;
-		int ygrid = (int) ycount/grid;
+		System.out.println("Each block contain matrix " + xgrid + "*" + ygrid);
 
 		SparkConf sparkConf = new SparkConf().setAppName("Gradient"); //.setMaster("local");
 		JavaSparkContext ctx = new JavaSparkContext(sparkConf);
@@ -66,8 +72,45 @@ public class GradientOnSpark{
 		//затем выбирается минимум из всех блоков
 		MBlock result = block_matrice.map(x -> x.compute())
 		.min(new MBlockComparator());
+
+		minvalue = result.min;
+		imin = result.xmin;
+		jmin = result.ymin;
 		
 		//вывод результата
 		System.out.println("Min = " + result.min + " x = " + result.xmin + " y = " + result.ymin); 
+
+		write();
 	}
+
+		
+	public static void write() {
+		//тестовый вывод в файл
+		File file = new File("output.txt");
+		try {
+	        //проверяем, что если файл не существует то создаем его
+	        if(!file.exists()){
+	            file.createNewFile();
+        	}
+ 
+	        //PrintWriter обеспечит возможности записи в файл
+	        PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+	        System.out.println("FilePath: " + file.getAbsolutePath() );
+	 
+	        try {
+	            //Записываем текст у файл
+	            out.println("Create matrix " + xcount + "*" + ycount + " elements and " + grid + "*" + grid + " blocks");
+	            out.println("Each block contain matrix " + xgrid + "*" + ygrid);
+	            out.println("Min = " + minvalue + " x = " + imin + " y = " + jmin);
+	        } finally {
+	            //После чего мы должны закрыть файл
+	            //Иначе файл не запишется
+	            out.close();
+	        }
+	    } catch(IOException e) {
+	        throw new RuntimeException(e);
+	    }
+
+	}
+
 }
